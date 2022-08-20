b3072eeb10fd201cdc4bc8655cc1b4d1b6c77c05
youngAaaaa <97266875+youngAaaaa@users.noreply.github.com>
2022-08-20 13:32:05 +0900

    [init] Project 초기 설정

---
+++ bunjang_ios_day/bunjang_ios_day/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bunjang_ios_day
+//
+//  Created by 안다영 on 2022/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
